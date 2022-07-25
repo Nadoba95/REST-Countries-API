0f3eb9e9a04eb8935aa6e34866316e3a4ef90595
Nadoba95 <dejannadobaa@gmail.com>
2022-07-25 19:11:58 +0200

    added arrow to go to the top of the page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import { useThemeContext } from "../../context/theme-context";
 import classes from "./Header.module.css";
 
 function Header() {
+  const [showArrow, setShowArrow] = useState(false);
   const navigate = useNavigate();
   const { isLight, changeTheme } = useThemeContext();
 
@@ -11,22 +12,47 @@ function Header() {
     navigate("/countries");
   }
 
+  function goToTopHandler() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
+  function showArrowHandler() {
+    if (window.scrollY > 200) {
+      setShowArrow(true);
+    } else {
+      setShowArrow(false);
+    }
+  }
+
+  window.addEventListener("scroll", showArrowHandler);
+
   const headerClasses = `${classes.header} ${!isLight && classes.active}`;
+  const arrowUpClasses = `${classes["arrow-up"]} ${
+    showArrow && classes.shown
+  } ${!isLight && classes.active}`;
 
   return (
-    <div className={headerClasses}>
-      <div>
-        <h1 onClick={goToStartingPageHandler}>Where in the world?</h1>
-        <p onClick={changeTheme}>
-          {isLight ? (
-            <i className="fa-solid fa-moon"></i>
-          ) : (
-            <i className="fa-solid fa-sun"></i>
-          )}
-          {isLight ? "Dark mode" : "Light mode"}
-        </p>
+    <>
+      <div className={headerClasses}>
+        <div>
+          <h1 onClick={goToStartingPageHandler}>Where in the world?</h1>
+          <p onClick={changeTheme}>
+            {isLight ? (
+              <i className="fa-solid fa-moon"></i>
+            ) : (
+              <i className="fa-solid fa-sun"></i>
+            )}
+            {isLight ? "Dark mode" : "Light mode"}
+          </p>
+        </div>
       </div>
-    </div>
+      <div className={arrowUpClasses} onClick={goToTopHandler}>
+        <i className="fa-solid fa-arrow-up"></i>
+      </div>
+    </>
   );
 }
 
